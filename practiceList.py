85d4c45d5d4f2268715780196e5b44a81b5cad07
ASHUTOSH SWAIN <ashutoshswain465@gmail.com>
2026-03-12 18:28:39 +0530

    List description

---
+++ practiceList.py
@@ -0,0 +1,8 @@
+print('Lists are used to store multiple items in a single variable.')
+print('Lists are created using square brackets.')
+
+mylist = ['apple', 'banana', 'cherry']
+print(f'\nthis is a list: {mylist}\n')
+
+print('List items are ordered, changeable and allows duplicate.')
+print('List index starts from 0')
