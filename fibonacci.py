54ff1c06a0f969a18f7751385f1089be5e093274
ASHUTOSH SWAIN <ashutoshswain465@gmail.com>
2026-03-30 09:58:35 +0530

    Code practice for fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+def fibonacci(num):
+    if num <= 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return fibonacci(num - 1) + fibonacci(num - 2)
+
+
+n = int(input('n: '))
+
+fib = fibonacci(n)
+print(fib)
